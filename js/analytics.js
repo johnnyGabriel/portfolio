7da1c35f6b92b83385d53716bd84e769457de1db
Johnny Gabriel <johnny_gabriel@outlook.com>
2014-09-06 13:12:47 -0300

    Ajustes w3c validator e analytics ajustado

--- js/analytics.js
+++ js/analytics.js
@@ -1,7 +1,12 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-52136840-1', 'hol.es');
-	  ga('send', 'pageview');
+	 ga('create', 'UA-52136840-1', 'hol.es');
+	 ga('send', 'pageview');
+
+	 $('ul').click(function(e) {
+		var elem = e.currentTarget.children[0].id;
+		ga('send', 'event', elem, 'click', 'trabalhos', 1);
+	});
