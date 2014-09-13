8febf618e4a45952f9954c95c4ef6eb2e6d75196
Johnny Gabriel <johnny_gabriel@outlook.com>
2014-09-13 00:10:04 -0300

    Implementação da Galeria
    Implementação da Galeria

--- snoopdogpets/js/gallery.js
+++ snoopdogpets/js/gallery.js
@@ -1,104 +1,125 @@
-	$(document).ready(function() {
+var gallery = {
 
-		$('#gallery img').click(function(e) {
+	//array com imagens
+	imagens: [],
 
-			gallery.create(e);
-			gallery.show();
+	//imagem selecionada
+	atual: 0,
 
-		});
+	//elementos graficos
+	container: "",
+	visualizador: "",
+	visualizador_img: "",
+	seta_prox: "",
+	seta_ant: "",
+	btn_fec: "",
+	btn_max: "",
 
-		$('#close').click(function() {
+	//velocidade transicao
+	speed: 500,
 
-			gallery.hide();
+	init: function(_container, _visualizador, _seta_prox, _seta_ant, _btn_fec, _btn_max) {
 
-		});
+		//inicializa as variaveis
+		this.container = _container;
+		this.visualizador = _visualizador;
+		this.seta_prox = _seta_prox;
+		this.seta_ant = _seta_ant;
+		this.btn_fec = _btn_fec;
+		this.btn_max = _btn_max;
 
-		$('#expand').click(function() {
-
-			gallery.expand();
-
-		});
-
-		$('#previous').click(function() {
-
-			gallery.prev();
-
-		});
-
-		$('#next').click(function() {
-
-			gallery.next();
-
-		});
-
-
-
-		var gallery = {
-
-			obj_img: $('#image-viewer').find('img'),
-			selected: "",
-			imgs: [],
-
-			create: function(e)
-			{
-				this.imgs = $('#gallery').find('img').toArray();
-				var s = e.target.src;
-				var i = 0;
-				while(s != this.imgs[i].src)
-				{
-					i++;
-				}
-				this.selected = i;
-				$(this.obj_img).attr('src', this.replaceURL(this.imgs[this.selected].src));
-			},
-
-			prev: function()
-			{
-				var new_selected = this.selected-1;
-
-				if(new_selected < 0)
-				{
-					new_selected = this.imgs.length-1;
-				}
-
-				$(this.obj_img).attr('src', this.replaceURL(this.imgs[new_selected].src));
-				this.selected = new_selected;
-			},
-
-			next: function()
-			{
-				var new_selected = this.selected+1;
-
-				if(new_selected > this.imgs.length-1)
-				{
-					new_selected = 0;
-				}
-
-				$(this.obj_img).attr('src', this.replaceURL(this.imgs[new_selected].src));
-				this.selected = new_selected;
-			},
-
-			show: function()
-			{
-				$('#image-viewer').fadeIn();
-			},
-
-			hide: function()
-			{
-				$('#image-viewer').fadeOut();
-			},
-
-			expand: function()
-			{
-				var path = $(this.obj_img).attr('src');
-				window.open(path, '_blank');
-			},
-
-			replaceURL: function(_url)
-			{
-				return _url.replace('thumb/', '');
-			}
-
+		//marca thumbs com ids
+		var thumbs = $(this.container).find('img');
+		for(i=0; x=thumbs.length, i < x; i++)
+		{
+			$(thumbs[i]).attr('id', 'img-'+i);
 		}
 
-	});
+		//inicializa o vetor
+		this.imagens = thumbs.toArray();
+
+		//inicializa visualizador
+		$(this.visualizador).append('<img src="" alt="" />');
+		this.visualizador_img = $(this.visualizador).find('img');
+
+		//evento de click nas thumbs
+		$(this.container+' img').click(function(e) {
+			
+			gallery.open(e.target.id.substring(4));
+
+		});
+
+	},
+
+	open: function(_id) {
+
+		if(typeof(_id) == 'undefined')
+		{
+			this.setAtual(0);
+
+		} else {
+
+			this.setAtual(_id);			
+		}
+		console.log(this.atual);
+		$(this.visualizador_img).attr('src', this.getFullImageURL(this.imagens[this.atual].src));
+		$(this.visualizador).fadeIn(this.speed);
+		
+		
+	},
+
+	close: function() {
+
+		$(this.visualizador).fadeOut(this.speed);
+		close();
+	},  
+
+	prox: function() {
+
+		if(this.atual == this.imagens.length-1)
+		{
+			this.atual = 0;
+
+		} else {
+			this.atual++;
+		}
+
+		this.open(this.atual);
+	},
+
+	ant: function() {
+
+		if(this.atual == 0)
+		{
+			this.atual = this.imagens.length-1;
+
+		} else {
+			this.atual--;
+		}
+
+		this.open(this.atual);
+	},
+
+	maximize: function() {
+
+		var path = gallery.imagens[gallery.atual].src;
+		window.open(path, '_blank');
+
+	},
+
+	setAtual: function(_id) {
+
+		this.atual = _id;
+
+	},
+
+	getFullImageURL:function(_url) {
+
+		return _url.replace('thumbs/', '');
+
+	}
+
+
+
+
+}
