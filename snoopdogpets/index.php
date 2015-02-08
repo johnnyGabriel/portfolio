81bf8f0eca002199e0a83804ddfebb2a5484eefc
Johnny Gabriel <johnny_gabriel@outlook.com>
2015-02-08 21:03:19 -0200

    Adicionada Gallery 2.0

--- snoopdogpets/index.php
+++ snoopdogpets/index.php
@@ -1,6 +1,6 @@
 <?php
 
-	include "php/mySQL.class.php";
+	include "../php/mySQL.class.php";
 
 	$bd = new mySQL;
 	$bd->selecionar_bd('u321650215_works');
@@ -17,23 +17,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
 	<link rel="icon" href="../images/favicon.ico" type="image/x-icon" />
-	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300italic,700,300' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="../css/projetos.css" type="text/css" />
+	<link rel="stylesheet" href="../Gallery.2.0/css/gallery.min.css"  type="text/css" />
+	<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300italic,700,300' type='text/css' />
 </head>
 
 	<body>
 
-		<div id="image-container">
-
-			<div class="left"></div>
-			<div class="right"></div>
-			<span>X</span>
-
+		<div id="gallery">
 		</div>
 
 		<div class="container">
 
-			<div class="header"></div>
+			<div style="background: url('img/cover.jpg')" class="header"></div>
 
 			<div class="header-shadow"></div>
 
@@ -50,7 +46,7 @@
 
 					<h2 id="nav-galeria">Galeria</h2>
 					<hr>
-					<div id="gallery">
+					<div id="gallery-thumbs">
 						<? echo $res->galeria ?>
 					</div>
 
@@ -59,16 +55,16 @@
 			</div>
 		</div>
 
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/gallery.min.js"></script>
-		<script type="text/javascript" src="js/jquery-scrolltofixed-min.js"></script>
+		<script type="text/javascript" src="../js/jquery.1.11.1.min.js"></script>
+		<script type="text/javascript" src="../js/jquery-scrolltofixed-min.js"></script>
+		<script type="text/javascript" src="../Gallery.2.0/js/gallery.min.js"></script>
 		<script type="text/javascript">
 
 			$(document).ready(function() {
 
 				/*
 
-				EVENTOS DO MENU
+				Menu Fixed
 
 				*/
 
@@ -105,41 +101,62 @@
 
 				});
 
+
 				/*
 
-				EVENTOS DA GALERIA
-
+				Galeria
+				
 				*/
 
-				//inicializa galeria
-				gallery.init('#image-container', '#gallery');
+				//inicialização da galeria
+				var imgs = [],
+	        		container = $('#gallery');
 
-				//seta velocidade da transição
-				gallery.speed = 300
-
-				//seta aspect ratio das imagens
-				gallery.setRatio(16, 9.61);
-
-				//seta esquerda				
-				$('#image-container .left').click(function() {
-
-					gallery.back();
-
+	        	$.each($('#gallery-thumbs').find('img'), function(i, val) {
+					imgs.push(val.src.replace('thumbs/', ''));
 				});
 
-				//seta direita
-				$('#image-container .right').click(function() {
+        		gallery.init({
+        			'container': container,
+        			'images': imgs,
+        			'loader': '../images/loader.gif',
+        			'speed': 300,
+        			'maxWidth': 1280
+        		});
 
-					gallery.forth();
+        		//evento de click das thumbs
+        		$('#gallery-thumbs img').on('click', function(e) {
 
-				});
+        			$('.container').fadeOut('fast');
+        			$('#gallery').fadeIn('fast', function() {
 
-				//botao fechar
-				$('#image-container span').click(function() {
+        				var id = (e.currentTarget.src).split('/');
+	        			id = id[id.length-1].split('.')[0];
 
-					gallery.close();
+	        			gallery.open(parseInt(id-1));
 
-					//IE Bug Fix
+        			});
+
+        			
+
+        			setTimeout(function() {
+
+        				var html =  "<span class='btn-close'>"+
+        								"<img src='../images/Close-icon.png' />"
+        							"</span>";
+        				$('#gallery').prepend(html);
+
+        				//evento do botão fechar galeria
+		        		$('.btn-close img').on('click', function() {
+		        			$('#gallery').fadeOut('normal');
+		        			$('.container').fadeIn('normal');
+		        		});
+
+        			}, 1000);
+
+        		});
+
+				/*	//IE Bug Fix
 					if(navigator.userAgent.search('Trident') > -1) {
 
 						$('.header').fadeOut(0, function() {
@@ -147,7 +164,7 @@
 						});
 					}
 
-				});
+				});*/
 
 			});
 
